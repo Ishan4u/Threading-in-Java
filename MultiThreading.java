5dc1f932671ee454f5152ff5d9b064e8d40b2393
Ishan4u <mohamedishan44@gmail.com>
2023-07-11 12:18:15 +0530

    Added two classes and created objects

--- MultiThreading.java
+++ MultiThreading.java
@@ -1,16 +1,41 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package multithreading;
 
 /**
  *
- * @author linux
+ * MultiThreading
  */
+class Book {
+
+    void updateDb() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Updating Db");
+        }
+    }
+    
+}
+
+class Num {
+
+    void print() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
 public class MultiThreading {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) { //thread
+        
+        //creating object for the class
+        Book book = new Book();
+        Num num = new Num();
+        
+        book.updateDb();
+        num.print();
+        
     }
 }
