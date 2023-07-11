3e5fb73d7cc1d6bdc23d1886ed0298d1146bf3b1
Ishan4u <mohamedishan44@gmail.com>
2023-07-11 10:00:15 +0530

    Add files

---
+++ MultiThreading.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package multithreading;
+
+/**
+ *
+ * @author linux
+ */
+public class MultiThreading {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
